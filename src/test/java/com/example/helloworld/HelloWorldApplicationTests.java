0a003d72649eae02a4e8ff311de064cf3e58058c
sankarElmsley <sankarraosiri@gmail.com>
2025-01-27 12:54:18 -0500

    Update HelloWorldApplicationTests.java

--- src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -19,218 +19,109 @@ class HelloWorldApplicationTests {
         // to ensure that controller is getting created inside the application context
         assertNotNull(helloWorldController);
     }
+}
+
+
 
 
 @Service
-@Transactional
 @Slf4j
-public class LocationProcessor {
-    
-    private final ConnectionManager ejcm;
-    
-    public LocationProcessor(ConnectionManager ejcm) {
-        this.ejcm = ejcm;
-    }
+public class LocationValueGenerator {
 
-    /**
-     * Process a single location with all its associated location values
-     */
-    public void processLocationWithValues(EDILocationRecord locationRec, String recNo, boolean isHomeowner) {
-        try {
-            // 1. Create and save the parent location first
-            EDILOCATION location = createLocation(locationRec, recNo);
-            int locationNo = saveLocation(location);
-
-            // 2. Create all associated location values referencing this location
-            List<EDILOCVAL> locationValues = new ArrayList<>();
-            
-            if (isHomeowner) {
-                // For homeowners, handle HSP/SLC values
-                locationValues.addAll(createHomeownerLocationValues(locationRec, recNo, locationNo));
-            } else {
-                // For commercial, handle BI form values
-                locationValues.addAll(createCommercialLocationValues(locationRec, recNo, locationNo));
-            }
-
-            // 3. Save all location values
-            saveLocationValues(locationValues);
-            
-            log.info("Processed location {} with {} location values", locationNo, locationValues.size());
-
-        } catch (SQLException e) {
-            log.error("Error processing location for recNo {}: {}", recNo, e.getMessage());
-            throw new LocationProcessingException("Failed to process location", e);
-        }
-    }
-
-    private EDILOCATION createLocation(EDILocationRecord locationRec, String recNo) {
-        EDILOCATION location = new EDILOCATION();
-        
-        // Set basic location info
-        location.setEDIRECNO(Integer.parseInt(recNo));
-        location.setEDILOCNO(locationRec.getLocationNo());
-        location.setEDILOCADD(locationRec.getAddress());
-        
-        // Set location description if applicable
-        if (StringUtils.isEmpty(location.getEDILOCADD()) && 
-            StringUtils.isNotEmpty(locationRec.LocLocationDesc)) {
-            String desc = locationRec.LocLocationDesc;
-            if (desc.length() > 64) {
-                location.setEDILOCADD(desc.substring(0, 62) + "**");
-                location.setEDILOCLOCATIONDESC(desc);
-            } else {
-                location.setEDILOCADD(desc);
-            }
-        }
-
-        // Set business fields
-        location.setEDILOCNAME("Building #" + locationRec.LocName);
-        location.setEDILOCBUSCODE(locationRec.LocBusCode);
-        location.setEDILOCCOV(locationRec.LocBMCov);
-        location.setEDILOCBIFORM(locationRec.LocBIForm1); // Primary BI form
-
-        // Set limits and values
-        if (!"NULL".equals(locationRec.LocDeduct)) {
-            location.setEDILOCDEDUCT(Double.parseDouble(locationRec.LocDeduct));
-        }
-        if (!"NULL".equals(locationRec.LocBuildingLimit)) {
-            location.setEDILOCBVALUE(Double.parseDouble(locationRec.LocBuildingLimit));
-        }
-        if (!"NULL".equals(locationRec.LocContentsLimit)) {
-            location.setEDILOCCVALUE(Double.parseDouble(locationRec.LocContentsLimit));
-        }
-
-        // Set audit fields
-        location.setEDICDATE(getCurrentTimestamp());
-        location.setEDIUDATE(getCurrentTimestamp());
-        location.setEDICUSER(getCurrentUser());
-        location.setEDIUUSER(getCurrentUser());
-        
-        return location;
-    }
-
-    private List<EDILOCVAL> createHomeownerLocationValues(EDILocationRecord locationRec, 
-            String recNo, int locationNo) {
-        List<EDILOCVAL> values = new ArrayList<>();
+    public List<EDILocationValue> generateLocationValues(Location location, String recNo) {
+        List<EDILocationValue> values = new ArrayList<>();
         int locVal = 0;
-        String bmCover = locationRec.LocBMCov;
-
-        // Create HSP value if applicable
-        if ("HSP".equals(bmCover) || "HSP/SLC".equals(bmCover)) {
-            values.add(createHSPSLCValue(locationRec, recNo, locationNo, ++locVal, true));
+        
+        // For Homeowners
+        if (LocationType.HOMEOWNER.equals(location.getLocationType())) {
+            String bmCover = location.getLocBMCov();
+            
+            if (SGIConstants.HSP.equals(bmCover)) {
+                values.add(createHSPSLCValue(location, recNo, ++locVal, bmCover, true));
+            } else if (SGIConstants.SLC.equals(bmCover)) {
+                values.add(createHSPSLCValue(location, recNo, ++locVal, bmCover, false));
+            } else if (SGIConstants.HSPSLC.equals(bmCover)) {
+                values.add(createHSPSLCValue(location, recNo, ++locVal, bmCover, true));  // HSP
+                values.add(createHSPSLCValue(location, recNo, ++locVal, bmCover, false)); // SLC
+            }
         }
-
-        // Create SLC value if applicable
-        if ("SLC".equals(bmCover) || "HSP/SLC".equals(bmCover)) {
-            values.add(createHSPSLCValue(locationRec, recNo, locationNo, ++locVal, false));
+        // For Commercial - Create value for each BI Form present
+        else {
+            if (hasValidBIForm(location.getLocBIForm1(), location.getLocBILimit1())) {
+                values.add(createBIFormValue(location, recNo, ++locVal, 
+                    location.getLocBIForm1(), location.getLocBILimit1(), location.getLocBuildingLimit()));
+            }
+            if (hasValidBIForm(location.getLocBIForm2(), location.getLocBILimit2())) {
+                values.add(createBIFormValue(location, recNo, ++locVal,
+                    location.getLocBIForm2(), location.getLocBILimit2(), location.getLocBuildingLimit()));
+            }
+            if (hasValidBIForm(location.getLocBIForm3(), location.getLocBILimit3())) {
+                values.add(createBIFormValue(location, recNo, ++locVal,
+                    location.getLocBIForm3(), location.getLocBILimit3(), location.getLocBuildingLimit()));
+            }
+            if (hasValidBIForm(location.getLocBIForm4(), location.getLocBILimit4())) {
+                values.add(createBIFormValue(location, recNo, ++locVal,
+                    location.getLocBIForm4(), location.getLocBILimit4(), location.getLocBuildingLimit()));
+            }
+            if (hasValidBIForm(location.getLocBIForm5(), location.getLocBILimit5())) {
+                values.add(createBIFormValue(location, recNo, ++locVal,
+                    location.getLocBIForm5(), location.getLocBILimit5(), location.getLocBuildingLimit()));
+            }
+            if (hasValidBIForm(location.getLocBIForm6(), location.getLocBILimit6())) {
+                values.add(createBIFormValue(location, recNo, ++locVal,
+                    location.getLocBIForm6(), location.getLocBILimit6(), location.getLocBuildingLimit()));
+            }
         }
-
+        
         return values;
     }
 
-    private List<EDILOCVAL> createCommercialLocationValues(EDILocationRecord locationRec, 
-            String recNo, int locationNo) {
-        List<EDILOCVAL> values = new ArrayList<>();
-        int locVal = 0;
-
-        // Check each BI form and create corresponding location value
-        if (isValidBIForm(locationRec.LocBIForm1, locationRec.LocBILimit1)) {
-            values.add(createBIFormValue(locationRec, recNo, locationNo, ++locVal,
-                locationRec.LocBIForm1, locationRec.LocBILimit1));
-        }
-        if (isValidBIForm(locationRec.LocBIForm2, locationRec.LocBILimit2)) {
-            values.add(createBIFormValue(locationRec, recNo, locationNo, ++locVal,
-                locationRec.LocBIForm2, locationRec.LocBILimit2));
-        }
-        if (isValidBIForm(locationRec.LocBIForm3, locationRec.LocBILimit3)) {
-            values.add(createBIFormValue(locationRec, recNo, locationNo, ++locVal,
-                locationRec.LocBIForm3, locationRec.LocBILimit3));
-        }
-        // ... handle forms 4-6 similarly
-
-        return values;
-    }
-
-    private EDILOCVAL createHSPSLCValue(EDILocationRecord locationRec, String recNo, 
-            int locationNo, int locVal, boolean isHSP) {
-        EDILOCVAL value = new EDILOCVAL();
-        
-        // Set parent location reference
-        value.setEDIRECNO(Integer.parseInt(recNo));
-        value.setEDILOCNO(locationNo);
-        value.setEDILOCVAL(locVal);
-        value.setEDILOCBMCOV("0");
+    private EDILocationValue createHSPSLCValue(Location location, String recNo, 
+            int locVal, String bmCover, boolean isHSP) {
+            
+        EDILocationValue.EDILocationValueBuilder builder = EDILocationValue.builder()
+            .ediRecNo(Integer.parseInt(recNo))
+            .locNo(Integer.parseInt(location.getLineNumber()))
+            .locVal(locVal)
+            .locBMCov("0");
 
         if (isHSP) {
-            value.setEDILOCAPREM(parseDouble(locationRec.LocHSPFTPrem));
-            value.setEDILOCPREMIUM(parseDouble(locationRec.LocHSPPremWrit));
-            value.setEDILOCCOMMISSION(parseDouble(locationRec.LocHSPComm));
-            value.setEDILOCDEDUCT(parseDouble(locationRec.LocHSPDeduct));
-            value.setEDILOCILVALUE(parseDouble(locationRec.LocHSPLimit));
-            value.setEDILOCPROPTYPE("HSP");
+            builder
+                .locAPrem(location.getLocHSPFTPrem())
+                .locPremium(location.getLocHSPPremWrit())
+                .locCommission(location.getLocHSPComm())
+                .locDeduct(location.getLocHSPDeduct())
+                .locILValue(location.getLocHSPLimit())
+                .locPropType(SGIConstants.HSP);
         } else {
-            value.setEDILOCAPREM(parseDouble(locationRec.LocSLCFTPrem));
-            value.setEDILOCPREMIUM(parseDouble(locationRec.LocSLCPremWrit));
-            value.setEDILOCCOMMISSION(parseDouble(locationRec.LocSLCComm));
-            value.setEDILOCDEDUCT(parseDouble(locationRec.LocSLCDeduct));
-            value.setEDILOCILVALUE(parseDouble(locationRec.LocSLCLimit));
-            value.setEDILOCPROPTYPE("SLC");
+            builder
+                .locAPrem(location.getLocSLCFTPrem())
+                .locPremium(location.getLocSLCPremWrit())
+                .locCommission(location.getLocSLCComm())
+                .locDeduct(location.getLocSLCDeduct())
+                .locILValue(location.getLocSLCLimit())
+                .locPropType(SGIConstants.SLC);
         }
 
-        // Set audit fields
-        value.setEDICDATE(getCurrentTimestamp());
-        value.setEDIUDATE(getCurrentTimestamp());
-        value.setEDICUSER(getCurrentUser());
-        value.setEDIUUSER(getCurrentUser());
-
-        return value;
+        return builder.build();
     }
 
-    private EDILOCVAL createBIFormValue(EDILocationRecord locationRec, String recNo, 
-            int locationNo, int locVal, String biForm, String biLimit) {
-        EDILOCVAL value = new EDILOCVAL();
-        
-        // Set parent location reference
-        value.setEDIRECNO(Integer.parseInt(recNo));
-        value.setEDILOCNO(locationNo);
-        value.setEDILOCVAL(locVal);
-        
-        // Set BI form specific values
-        value.setEDILOCBIFORM(lookupCoverageCodeDesc(biForm));
-        value.setEDILOCBILIMIT(Double.parseDouble(biLimit));
-        value.setEDILOCBIVAL(parseDouble(locationRec.LocBuildingLimit));
-        value.setEDILOCPROPTYPE("BI");
-
-        // Set audit fields
-        value.setEDICDATE(getCurrentTimestamp());
-        value.setEDIUDATE(getCurrentTimestamp());
-        value.setEDICUSER(getCurrentUser());
-        value.setEDIUUSER(getCurrentUser());
-
-        return value;
+    private EDILocationValue createBIFormValue(Location location, String recNo, 
+            int locVal, String biForm, Double biLimit, Double buildingValue) {
+            
+        return EDILocationValue.builder()
+            .ediRecNo(Integer.parseInt(recNo))
+            .locNo(Integer.parseInt(location.getLineNumber()))
+            .locVal(locVal)
+            .locBIForm(lookupCoverageCodeDesc(biForm))
+            .locBILimit(biLimit)
+            .locBIVal(buildingValue)
+            .locPropType("BI")
+            .build();
     }
 
-    private int saveLocation(EDILOCATION location) throws SQLException {
-        ejcm.writeRow(location);
-        log.info("Saved location - RecNo: {}, LocNo: {}", 
-            location.getEDIRECNO(), location.getEDILOCNO());
-        return location.getEDILOCNO();
-    }
-
-    private void saveLocationValues(List<EDILOCVAL> values) throws SQLException {
-        for (EDILOCVAL value : values) {
-            ejcm.writeRow(value);
-            log.info("Saved location value - RecNo: {}, LocNo: {}, LocVal: {}", 
-                value.getEDIRECNO(), value.getEDILOCNO(), value.getEDILOCVAL());
-        }
-    }
-
-    private boolean isValidBIForm(String form, String limit) {
-        return !"NULL".equals(form) && !"NULL".equals(limit);
-    }
-
-    private Double parseDouble(String value) {
-        return "NULL".equals(value) ? 0.0 : Double.parseDouble(value);
+    private boolean hasValidBIForm(String form, Double limit) {
+        return StringUtils.isNotEmpty(form) && limit != null;
     }
 
     private String lookupCoverageCodeDesc(String code) {
@@ -246,5 +137,4 @@ public class LocationProcessor {
         );
         return coverageCodes.getOrDefault(code, code);
     }
-}
 }
